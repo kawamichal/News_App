0d45b67a980e9586564cbd0b8c7fce19a7108f47
Michał Kawa <michal.kawa@hotmail.com>
2019-10-10 16:49:08 +0200

    Detail view added

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from posts.views import BlogListView
+from posts.views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>', BlogDetailView.as_view(), name="post_detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from posts.models import Post
 
@@ -9,3 +9,7 @@ from posts.models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'posts/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'posts/detail.html'
